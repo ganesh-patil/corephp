30966ee2679ea6ea0d1860f8855354a7c9c31ece
ganesh <ganesh@ganesh-desktop.(none)>
2013-05-01 16:44:55 +0545

    css path changed

--- php/index.php
+++ php/index.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title>Welcome to OpenShift</title>
-   <link href="css/custom.css" type"text/css"> </style>
+   <link rel="stylesheet" type="text/css" href="css/custom.css">
 </head>
 <body>
     <img class="brand-image"
