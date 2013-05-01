4083467071c40340be9379a5ad881c660b4891b4
ganesh <ganesh@ganesh-desktop.(none)>
2013-05-01 16:39:06 +0545

    changed index file

--- php/index.php
+++ php/index.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title>Welcome to OpenShift</title>
-   <style src="css/custom.css"> </style>
+   <link href="css/custom.css" type"text/css"> </style>
 </head>
 <body>
     <img class="brand-image"
